4e5644f1d83f047d25f18fc67064db4ed63345a5
Jeremy Cook <jeremy.cook.llb@live.com>
2016-10-02 23:07:57 +1300

    Creating db context

---
+++ FRWP/DAL/RefereeContext.cs
@@ -0,0 +1,26 @@
+﻿using FRWP.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace FRWP.DAL
+{
+    public class RefereeContext : DbContext
+    {
+        public RefereeContext() : base("RefereeContext")
+        {
+        }
+
+        public DbSet<Game> Games { get; set; }
+        public DbSet<GamePlayer> GamePlayers { get; set; }
+        public DbSet<Player> Players { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
